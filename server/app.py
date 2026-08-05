0260430ff6fe72efcf5481b1ade6eb45b05ec9c1
carol <carolineallen1012@gmail.com>
2026-08-05 19:18:04 -0400

    tests passing

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, request, session
+from flask import Flask, make_response, request, session
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
 
 from models import db, Article, User, ArticlesSchema, UserSchema
 
@@ -16,41 +15,51 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-api = Api(app)
+@app.route('/login', methods=['POST'])
+def login():
+    user = User.query.filter(
+        User.username == request.get_json()['username']
+    ).first()
+    if user:
+        session['user_id'] = user.id
+        return UserSchema().dump(user)
+    return {'message': 'Invalid login'}, 401
 
-class ClearSession(Resource):
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    session['user_id'] = None
+    return {'message': '204: No Content'}, 204
 
-    def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+@app.route('/check_session', methods=['GET'])
+def check_session():
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.filter(User.id == user_id).first()
+        return UserSchema().dump(user), 200
+    return {}, 401
 
-        return {}, 204
+@app.route('/clear', methods=['DELETE'])
+def clear_session():
+    session['page_views'] = None
+    session['user_id'] = None
+    return {}, 204
 
-class IndexArticle(Resource):
-    
-    def get(self):
-        articles = [ArticlesSchema().dump(article) for article in Article.query.all()]
-        return articles, 200
+@app.route('/articles', methods=['GET'])
+def index_articles():
+    articles = [ArticlesSchema().dump(article) for article in Article.query.all()]
+    return articles, 200
 
-class ShowArticle(Resource):
+@app.route('/articles/<int:id>', methods=['GET'])
+def show_article(id):
+    session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
+    session['page_views'] += 1
 
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
+    if session['page_views'] <= 3:
+        article = Article.query.filter(Article.id == id).first()
+        article_json = ArticlesSchema().dump(article)
+        return make_response(article_json, 200)
 
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = ArticlesSchema.dump(article)
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
+    return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
